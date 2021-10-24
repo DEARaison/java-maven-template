ea1880b26e598c949bee1a14ede73c7b950ff3f7
DEA Raison <mrbaotruongit@gmail.com>
2021-10-25 01:06:34 +0700

    Fix bug importing JUnit
    Signed-off-by: DEA Raison <mrbaotruongit@gmail.com>

--- src/test/java/com/github/dearaison/template/CalculatorTest.java
+++ src/test/java/com/github/dearaison/template/CalculatorTest.java
@@ -1,10 +1,12 @@
 package com.github.dearaison.template;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 /**
  * Created by IntelliJ on Sunday, 20 September, 2020 at 11:22.
