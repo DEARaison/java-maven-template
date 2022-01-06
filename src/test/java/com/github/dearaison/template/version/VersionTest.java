3f4ea84c2ad0a76f69cd28d28ea051a8d159d111
DEA Raison <DEARaison0@gmail.com@gmail.com>
2022-01-06 12:23:28 +0700

    Adding Version.java and deleting Calculator.java
    Signed-off-by: DEA Raison <DEARaison0@gmail.com@gmail.com>

---
+++ src/test/java/com/github/dearaison/template/version/VersionTest.java
@@ -0,0 +1,28 @@
+package com.github.dearaison.template.version;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+/**
+ * Created by IntelliJ on Thursday, 06 January, 2022 at 12:02.
+ *
+ * @author DEA Raison
+ */
+class VersionTest {
+
+    @Test
+    void getInstance() {
+        assertInstanceOf(Version.class, Version.getInstance());
+    }
+
+    @Test
+    void getVersion() throws IOException {
+        String version = Version.getInstance().getVersion();
+        assertEquals("1.0.0-SNAPSHOT", version);
+        System.out.println("Current version: " + version);
+    }
+}
