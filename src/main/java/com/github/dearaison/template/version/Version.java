8c0eeeaa32527732fb38c301da232025d274c69d
DEA Raison <DEARaison0@gmail.com@gmail.com>
2022-01-06 12:20:04 +0700

    Adding Version.java and deleting Calculator.java
    Signed-off-by: DEA Raison <DEARaison0@gmail.com@gmail.com>

---
+++ src/main/java/com/github/dearaison/template/version/Version.java
@@ -0,0 +1,34 @@
+package com.github.dearaison.template.version;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Created by IntelliJ on Thursday, 06 January, 2022 at 11:57.
+ *
+ * @author DEA Raison
+ */
+class Version {
+    private static final Version INSTANCE = new Version();
+
+    /**
+     * Get the singleton class's instance
+     *
+     * @return the singleton class's instance
+     */
+    static Version getInstance() {
+        return INSTANCE;
+    }
+
+    /**
+     * Get the current version which is defined in pom.xml.
+     *
+     * @return the current version
+     * @throws IOException if an error occurred when reading from the input stream.
+     */
+    String getVersion() throws IOException {
+        Properties properties = new Properties();
+        properties.load(getClass().getResourceAsStream("/project.properties"));
+        return properties.getProperty("project.version");
+    }
+}
